52134be2e7f46d3cf4cab44f52acc7596fef1a11
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-14 22:08:06 +0500

    added

--- script/main.js
+++ script/main.js
@@ -1,4 +1,7 @@
 const swipreWrapper = document.querySelector(".swiper-wrapper");
+const userId = JSON.parse(
+  atob(localStorage.getItem("login-token").split(".")[1])
+).id;
 
 fetch("http://localhost:3000/api/posts")
 .then(res => res.json())
@@ -11,7 +14,7 @@ fetch("http://localhost:3000/api/posts")
 function renderData(data){
     const renderFragment = document.createDocumentFragment();
     data.forEach(el => {
-        console.log(el._id);
+        // console.log(el._id);
         const div = document.createElement("div");
         div.classList = "swiper-slide";
         // setTimeout((div) => {
@@ -20,11 +23,12 @@ function renderData(data){
               <img src="${el.image}" alt="cards img" />
             </a>
             <div class="swiper-card-div">
-              <h2>${el.title.slice(0,10)}</h2>
+              <h2>${el.title.slice(0,20)}...</h2>
               <p>
-                ${el.description.slice(0,40)}
+                ${el.description.slice(0,30)}...
               </p>
-              <div>
+              <div class="div">
+                <h3 style="color:#fff; ">${localStorage.getItem("userName")}</h3>
                 <p>user</p>
               </div>
             </div>
@@ -33,3 +37,28 @@ function renderData(data){
     })
     swipreWrapper.appendChild(renderFragment)
 }
+
+let a = localStorage.getItem("login-token")
+const account = document.querySelector(".account")
+const dashboard = document.querySelector(".dashboard")
+if(a){
+  account.style.display = "none";
+}
+
+if(a){
+  dashboard.style.display = "block";
+}
+
+// userName = document.querySelector(".usernamee")
+
+// console.log(userName);
+
+fetch(`http://localhost:3000/api/users/${userId}`)
+  .then((res) => res.json())
+  .then((data) => {
+    localStorage.setItem("userName", data.data.fullname)
+    // $profilInfo.innerHTML = `
+    //     <strong >${data.data.fullname}</strong>
+    //     <small>User</small>
+    // `;
+  });
